ac312e5172876e3b3e3607c994b60af56594bc19
Angel Martin Alejandro Santiago <angelmartin.isc@gmail.com>
2020-02-12 18:20:43 -0600

    auth signup operation

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post, Body, Get, ValidationPipe } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+
+@Controller('auth')
+export class AuthController {
+
+    constructor(
+        private authService: AuthService
+    ){}
+
+    @Post('/signup')
+    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.authService.signUp(authCredentialsDto);
+    }
+
+}
