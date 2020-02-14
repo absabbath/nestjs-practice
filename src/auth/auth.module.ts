3644d68a0c7ec8a70db8be0f00383cf05643a674
Angel Martin Alejandro Santiago <angelmartin.isc@gmail.com>
2020-02-13 22:58:48 -0600

    jwt config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      secret: 'topSecret007',
+      signOptions: {
+        expiresIn: 3600,
+      }
+    }),
     TypeOrmModule.forFeature([ UserRepository ]),
   ],
   controllers: [AuthController],
