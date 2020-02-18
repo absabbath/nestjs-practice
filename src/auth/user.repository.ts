ca12f396c12bdac21e270427ef35f5ed8de52f1e
Angel Martin Alejandro Santiago <angelmartin.isc@gmail.com>
2020-02-18 17:11:14 -0600

    testing repository an jwtstrategy

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -10,7 +10,8 @@ export class UserRepository extends Repository<User> {
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
         
-        const user = new User();
+        const user = this.create();
+        
         user.username = username;
         user.salt = await bcrypt.genSalt();
         user.password = await this.hashPasssword(password, user.salt);
